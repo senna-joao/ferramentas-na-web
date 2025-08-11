79707eb6cbd8f718a5a64a3db81f0c92492b3f35
biffyclyro <joao-senna1@hotmail.com>
2025-08-11 12:23:04 -0300

    versão refatorada

---
+++ src/contador.d.ts
@@ -0,0 +1,2 @@
+export declare const contador: () => void;
+//# sourceMappingURL=contador.d.ts.map
---
+++ src/contador.ts
@@ -0,0 +1,77 @@
+const mainDiv = document.querySelector('#main')
+
+let contValue = 0
+
+const validKeys = new Map<string, () => void>([
+	['+', () => {
+		contValue++
+		updateValue()
+	}],
+	['-', () => {
+		contValue--
+		updateValue()
+	}],
+	['Delete', () => {
+		contValue = 0
+		updateValue()
+	}]
+])
+
+const inputFieldCreator = () => {
+	const inputField = document.createElement('input')
+	inputField.setAttribute('type', 'number')
+	inputField.setAttribute('id', 'input-field')
+	inputField.setAttribute('class', 'form-control text-end my-2')
+	inputField.value = String(contValue)
+	inputField.setAttribute('readonly', '')
+	mainDiv?.appendChild(inputField)
+}
+
+const updateValue = () => {
+		const inputField: HTMLInputElement | null = document.querySelector('#input-field')
+		if ( inputField) { inputField.value = String(contValue) } 
+}
+
+const createButton = (buttonsDiv: HTMLElement, buttonText: string, CSSclass: string, clickEventCallback: () => void) => {
+	const button = document.createElement('button')
+	button.setAttribute('class', CSSclass)
+	button.innerHTML = buttonText 
+	button.addEventListener('click', clickEventCallback)
+	buttonsDiv.appendChild(button)
+}
+
+const createButtonsDiv = () => {
+	const upperDiv = document.createElement('div')
+	const buttonsDiv = document.createElement('div')
+	buttonsDiv.setAttribute('id', 'buttons-div ')
+	upperDiv.appendChild(buttonsDiv)
+	mainDiv?.appendChild(upperDiv)
+
+	createButton(buttonsDiv, '+', 'btn btn-success col-lg-3 col-12 my-2 me-2', validKeys.get('+')!)
+	
+	createButton(buttonsDiv, '-', 'btn btn-primary col-lg-3 col-12 my-2 me-2', validKeys.get('-')!)
+
+	createButton(buttonsDiv, 'Reset', 'btn btn-danger col-lg-3 col-12 my-2', validKeys.get('Delete')!)
+}
+
+const createTitle = () => {
+	const title = document.createElement('h2')
+	title.setAttribute('class', 'justify-text-center')
+	title.innerHTML = 'Contador'
+	mainDiv?.appendChild(title)
+}
+
+const keyBoardEventHandler = (event: KeyboardEvent) => {
+	const key = event.key
+
+	if (validKeys.has(key)) {	validKeys.get(key)!()	}
+}
+
+
+export const contador = () => {
+	createTitle()
+	inputFieldCreator()
+	createButtonsDiv()
+
+	window.addEventListener('keydown', keyBoardEventHandler)
+}
